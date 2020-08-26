45039fbd8a399134eb55c23c2f665994d7041eb0
Guilmar Ciriaco Soto <guilmar.ciriaco@unas.edu.pe>
2020-08-26 17:24:12 -0500

    Jalando datos dfgre43

--- hostal_v_1/resources/views/home.blade.php
+++ hostal_v_1/resources/views/home.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -946,3 +947,28 @@
 
 </body>
 </html>
+=======
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+>>>>>>> borrado de registro
